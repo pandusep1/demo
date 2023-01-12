e2bc8cb14d2e65f425c6faec80a5d6e2aefa9605
Alyana Vandana <Alyana.Vandana@brillio.com>
2023-01-12 06:16:58 +0000

    Updated main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -236,7 +236,7 @@ resource "google_cloudfunctions_function_iam_binding" "invoker" {
   role = "roles/cloudfunctions.invoker"
   members = [
     "allUsers",
-    "terraformsa@pcialyana.iam.gserviceaccount.com"
+    "serviceAccount:terraformsa@pcialyana.iam.gserviceaccount.com"
     
   ]
 }
